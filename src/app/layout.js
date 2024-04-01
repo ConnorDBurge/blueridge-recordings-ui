6f698e8dc86130d5cd74eebe9544598a14292a94
Connor Burge <connor@blueridgerecordings.com>
2024-04-01 15:14:37 -0400

    added speed insight component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "@/app/globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Create Next App",
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
