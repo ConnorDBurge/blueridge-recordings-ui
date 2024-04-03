9d27e9b07aff413e28695166c45f9d561bef8df3
Connor Burge <connor@blueridgerecordings.com>
2024-04-03 12:33:40 -0400

    fixed middleware;

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 export function middleware(request) {
   if (request.nextUrl.pathname.startsWith("/brands")) {
     return NextResponse.rewrite(new URL("/", request.url));
