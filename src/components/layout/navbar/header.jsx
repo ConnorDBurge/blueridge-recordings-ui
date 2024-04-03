4880b57b19fffccb4f953d06b1d74a8d591780ef
Connor Burge <connor@blueridgerecordings.com>
2024-04-03 14:59:07 -0400

    moved most of navbar to server rendering

--- src/components/layout/navbar/header.jsx
+++ src/components/layout/navbar/header.jsx
@@ -1,17 +1,11 @@
 "use client";
 
-import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 
-import { SearchIcon, HeaderLogo } from "@/components/icons";
-import { Search } from "./search";
-import { ShoppingCartButton } from "./shopping-cart";
-
-export default function Header({ children, shop }) {
+export default function Header({ children, className }) {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const lastScrollY = useRef(0);
 
-  // Handle collapsing header on scroll
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
@@ -48,36 +42,11 @@ export default function Header({ children, shop }) {
 
   return (
     <header
-      className={`bg-colors-primary h-[60px] sticky top-0 z-10 py-2 md:pt-4 md:pb-0 transition-all duration-300 ease-in-out ${
+      className={`${
         isCollapsed ? "md:h-[83px]" : "md:h-[116px]"
-      }`}
+      }  ${className} h-[58px]`}
     >
-      <div className="container flex flex-col justify-between">
-        <div className="flex">
-          {/* <HamburgerButton /> */}
-          <Link
-            href="/"
-            className="block ml-14 mb-4 pt-[2px] w-[150px] md:w-[200px] md:p-0 md:ml-[10px]"
-          >
-            <HeaderLogo />
-          </Link>
-          <Search />
-          <div className="lg:w-[200px] md:w-[100px] h-[43px] mr-[6px] md:mt-1 flex justify-end items-center gap-4">
-            <SearchIcon className="md:hidden h-8 w-8 text-white hover:fill-colors-secondary hover:cursor-pointer" />
-            <ShoppingCartButton />
-          </div>
-        </div>
-        <div
-          className={`transition-all duration-300 ease-in-out ${
-            isCollapsed
-              ? "max-h-0 opacity-0 overflow-hidden transition-[max-height 300ms ease-in-out, opacity 300ms ease-in-out]"
-              : "max-h-[500px] opacity-1"
-          }`}
-        >
-          {children}
-        </div>
-        {/* <MobileMenu {...rest} className="md:hidden" /> */}
-      </div>
+      {children}
     </header>
   );
 }
--- src/components/layout/navbar/navbar.jsx
+++ src/components/layout/navbar/navbar.jsx
@@ -1,16 +1,26 @@
-import { getStorefront } from "@/lib/shopify";
+import { HeaderLogo, SearchIcon } from "@/components/icons";
 import AnnouncementBar from "./anncouncement-bar";
 import Header from "./header";
 import { MainMenu } from "./menu";
+import { Search } from "./search";
+import { ShoppingCartButton } from "./shopping-cart";
 
-export default async function Navbar() {
-  const shop = await getStorefront();
-
+export default function Navbar() {
   return (
     <>
       <AnnouncementBar />
-      <Header shop={shop}>
-        <MainMenu />
+      <Header className="bg-colors-primary h-[58px] sticky top-0 z-10 py-2 md:pt-4 md:pb-0 transition-all duration-300 ease-in-out overflow-hidden">
+        <div className="container flex flex-col justify-between">
+          <div className="flex">
+            <HeaderLogo />
+            <Search />
+            <div className="lg:w-[200px] md:w-[100px] h-[43px] mr-[6px] md:mt-1 flex justify-end items-center gap-4">
+              <SearchIcon className="md:hidden h-8 w-8 text-white hover:fill-colors-secondary hover:cursor-pointer" />
+              <ShoppingCartButton />
+            </div>
+          </div>
+          <MainMenu />
+        </div>
       </Header>
     </>
   );
