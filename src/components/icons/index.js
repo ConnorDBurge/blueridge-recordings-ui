594ec81f2d459121ddb932d801be98a3a182eb21
Connor Burge <connor@blueridgerecordings.com>
2024-04-03 13:46:00 -0400

    using SVG for header logo

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,3 +1,4 @@
 export { default as ChevronDownIcon } from "./chevron-down.jsx";
 export { default as SearchIcon } from "./search.jsx";
 export { default as ShoppingCartIcon } from "./shopping-cart.jsx";
+export { default as HeaderLogo } from "./header.jsx";
--- src/components/layout/navbar/header.jsx
+++ src/components/layout/navbar/header.jsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 
-import { SearchIcon } from "@/components/icons";
+import { SearchIcon, HeaderLogo } from "@/components/icons";
 import { Search } from "./search";
 import { ShoppingCartButton } from "./shopping-cart";
 
@@ -11,6 +11,7 @@ export default function Header({ children, shop }) {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const lastScrollY = useRef(0);
 
+  // Handle collapsing header on scroll
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
@@ -33,19 +34,22 @@ export default function Header({ children, shop }) {
     };
 
     const onScroll = () => {
+      console.log("scrolling");
       window.requestAnimationFrame(handleScroll);
     };
-    window.addEventListener("scroll", onScroll);
 
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-    };
+    if (window.innerWidth >= 769) {
+      window.addEventListener("scroll", onScroll);
+      return () => {
+        window.removeEventListener("scroll", onScroll);
+      };
+    }
   }, [isCollapsed]);
 
   return (
     <header
       className={`bg-colors-primary h-[60px] sticky top-0 z-10 py-2 md:pt-4 md:pb-0 transition-all duration-300 ease-in-out ${
-        isCollapsed ? "md:h-[83px]" : "md:h-[119px]"
+        isCollapsed ? "md:h-[83px]" : "md:h-[116px]"
       }`}
     >
       <div className="container flex flex-col justify-between">
@@ -55,7 +59,7 @@ export default function Header({ children, shop }) {
             href="/"
             className="block ml-14 mb-4 pt-[2px] w-[150px] md:w-[200px] md:p-0 md:ml-[10px]"
           >
-            <img src={shop?.brand?.logo?.image?.url} />
+            <HeaderLogo />
           </Link>
           <Search />
           <div className="lg:w-[200px] md:w-[100px] h-[43px] mr-[6px] md:mt-1 flex justify-end items-center gap-4">
